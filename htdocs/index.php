4b675a48f7f04b1e329f6349e285d408904dbbfd
Jeff Goodhue <goodhue.jeff@gmail.com>
2014-11-01 13:29:59 -0700

    register.php has a sign in form and sign up form. php scripts do not work quite yet, need to work with Matt's SQL landing.php is what will be shown when the user logs in

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,8 @@
 <?php
-include "/hackathonfall14/includes/register.php"
+session_start();
+if(isset($_SESSION['loggedin'] && $_SESSION['loggedin'] == true)) {
+    include "/hackathonfall14/includes/landing.php"
+} else {
+    include "/hackathonfall14/includes/register.php"
+}
 ?>
--- includes/register.php
+++ includes/register.php
@@ -1,13 +1,23 @@
 <html>
 <body>
-
+<!--Basically, forums just need to use post and you have to let Jeff know the name of the inputs -->
+<!--This is a shitty mockup from Jeff, change it however you want -->
 <form action="index.php" method="post">
+    <p>Sign up!</p>
     <p>Username: <input type="text" name="user" /></p>
     <p>Password: <input type="text" name="pass" /></p>
     <p>Email: <input type="text" name="email" /></p>
     <p><input type="submit" name="submit" value="Register" /></p>
     <input type="hidden" name="submitted" value="TRUE" />
 </form>
+<form action="index.php" method="post">
+    <p>Log in!</p>
+    <p>Username: <input type="text" name="userlog" /></p>
+    <p>Password: <input type="text" name="passlog" /></p>
+    <p><input type="submit" name="submitlog" value="Register" /></p>
+    <input type="hidden" name="submittedlog" value="TRUE" />
+</form>
+<!-- keep this php stuff -->
 <?php
 include "/hackathonfall14/includes/registersc.php";
 ?>
