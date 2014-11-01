0ccd626d21e927081ac6d4cfc16396254fe231bb
Matthew Ryan Lam <lamm3@quest-OptiPlex-9010>
2014-11-01 15:51:21 -0700

    sql file to fill in example data

---
+++ SQL/example/exampleDepartmentSet.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (code, name)
+VALUES ('CSCI', 'Computer Science');
+
+INSERT INTO department (code, name)
+VALUES ('BIOL', 'Biology');
+
+INSERT INTO department (code, name)
+VALUES ('CHEM', 'Chemistry');
+
+INSERT INTO department (code, name)
+VALUES ('ECON', 'Economics');
+
+INSERT INTO department (code, name)
+VALUES ('M/CS', 'Mathematics/Computer Science');
+
+INSERT INTO department (code, name)
+VALUES ('MATH', 'Mathematics');
+
+INSERT INTO department (code, name)
+VALUES ('PHYS', 'Physics');
+
+INSERT INTO department (code, name)
+VALUES ('ACCT', 'Accounting');
