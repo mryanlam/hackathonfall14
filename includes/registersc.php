a917ce542cda2256968e18fa1f8d2632bd1bf7e0
Jeff Goodhue <goodhue.jeff@gmail.com>
2014-11-01 15:19:55 -0700

    Missed semicolon

--- includes/registersc.php
+++ includes/registersc.php
@@ -29,7 +29,7 @@ if(isset($_POST['submittedlog'])) {
     }
     $query = "SELECT username, shaPassHash, first, last FROM account WHERE username=".$_POST['userlog'];
     if($result->num_rows > 0) {
-        $row = $result->fetch_assoc() 
+        $row = $result->fetch_assoc();
         if($row['shaPassHash'] = hash("sha512", $_POST['userlog'].$_POST['passlog']) {
             session_start();
             $_SESSION['loggedin'] = true;
