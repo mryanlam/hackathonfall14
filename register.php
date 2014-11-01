755056d4f70fb6771ccfb6eac0a9416ceb22f70a
Jeff Goodhue <goodhue.jeff@gmail.com>
2014-11-01 12:20:57 -0700

    include commit

---
+++ register.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<form action="index.php" method="post">
+    <p>Username: <input type="text" name="user" /></p>
+    <p>Password: <input type="text" name="pass" /></p>
+    <p>Email: <input type="text" name="email" /></p>
+    <p><input type="submit" name="submit" value="Register" /></p>
+    <input type="hidden" name="submitted" value="TRUE" />
+</form>
+<?php
+include "includes/registersc.php";
+?>
+</body>
+</html>
