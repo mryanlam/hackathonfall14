a3920bfb330828f96ce5f89074ed148c0edad4d1
Matthew Ryan Lam <lamm3@quest-OptiPlex-9010>
2014-11-01 13:48:01 -0700

    updates to SQL database schema

--- SQL/create/base.sql
+++ SQL/create/base.sql
@@ -37,3 +37,31 @@ CREATE TABLE `myCourses` (
     `crs4` int(10) unsigned,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Courses an account is associated with';
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Identifier',
+    `previousId` int(10) unsigned,  /* used for responces */
+    `timePosted` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `type` int(10) unsigned NOT NULL, /* 0 for text, 1 for event */
+    `authId` int(10) unsigned,
+    `crsId` int(10) unsigned,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Posts in a course';
+
+DROP TABLE IF EXISTS `event`;
+CREATE TABLE `event` (
+    `postId` int(10) unsigned NOT NULL,
+    `time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `title` varchar(32) NOT NULL DEFAULT 'Your Title',
+    `message` varchar(1000) NOT NULL DEFAULT 'description of the event',
+    `location` varchar(32) NOT NULL DEFAULT 'Location of the event',
+    PRIMARY KEY (`postId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Posts that are events';
+
+DROP TABLE IF EXISTS `textPost`;
+CREATE TABLE `textPost` (
+    `postId` int(10) unsigned NOT NULL,
+    `message` varchar(1000) NOT NULL DEFAULT 'Your Message',
+    PRIMARY KEY (`postId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Posts that are normal messages';
