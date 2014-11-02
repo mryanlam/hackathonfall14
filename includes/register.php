bbd404091922c5397deb2dd060de84744abdc3b9
Matthew Ryan Lam <lamm3@quest-OptiPlex-9010>
2014-11-02 02:30:23 -0800

    signin pw field is now properly tagged as a password

--- includes/register.php
+++ includes/register.php
@@ -16,7 +16,7 @@ include "/hackathonfall14/includes/header.php";
 <form action="index.php" method="post">
     <p>Log in!</p>
     <p><input type="text" name="userlog" placeholder="Username or Email"/></p>
-    <p><input type="text" name="passlog" placeholder="Password"/></p>
+    <p><input type="password" name="passlog" placeholder="Password"/></p>
     <p><input type="submit" name="submitlog" value="Login" /></p>
     <input type="hidden" name="submittedlog" value="TRUE" />
 </form>
