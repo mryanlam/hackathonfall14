cd9a9ee49d5b7f952f4e34d5eb6f858ecd8a6b30
Matthew Ryan Lam <lamm3@quest-OptiPlex-9010>
2014-11-01 19:18:54 -0700

    first try and the php for grabbing relevant info for adding classes

---
+++ includes/addclassessc.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST['submittedlog'])) {
+    DEFINE('USER', 'root');
+    DEFINE('PASSWORD', '');
+    DEFINE('HOST', 'localhost');
+    DEFINE('DB', 'auth');
+    $dbc = new mysqli(HOST, USER, PASSWORD, DB);
+    if($dbc->connect_error) {
+        trigger_error("Could not connect! ".$dbc->connect_error, E_USER_ERROR);
+    }
+    $query = "SELECT department.code, course.id, course.courseNumber, course.courseName FROM course INNER JOIN department ON course.deptid=department.id";
+    $result = $dbc->query($query);
+    if($result) {
+        while ($row = $result->fetch_assoc())
+        {
+            printf("%s, %d, %s\n", $row["department.code"], $row["course.courseNumber"], $row["course.courseName"]);
+        }
+    } else {
+        print("<p>No Classes Exist</p>");
+    }
+    $dbc->close();
+}
+?>
