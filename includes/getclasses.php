e5375664201cb59e04d3be8dba4467b8abaa7fda
Jeff Goodhue <goodhue.jeff@gmail.com>
2014-11-01 21:51:49 -0700

    Fixed array

--- includes/getclasses.php
+++ includes/getclasses.php
@@ -4,9 +4,9 @@ foreach($classes as $curclass) {
     $result = $dbc->query($query);
     if($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        print('<div class="class" id='.$row['course.courseNumber'].'>');
-        print($row['department.code'].' '.$row['course.courseNumber'].'<br>');
-        print($row['course.courseName']);
+        print('<div class="class" id='.$row['courseNumber'].'>');
+        print($row['code'].' '.$row['courseNumber'].'<br>');
+        print($row['courseName']);
         print('</div>');
     }
 }
