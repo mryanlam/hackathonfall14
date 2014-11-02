24cc920b8b55117725d0bcf3957ca814b4e7203c
Matthew Ryan Lam <lamm3@quest-OptiPlex-9010>
2014-11-01 19:33:31 -0700

    moved mission statement out of the header

--- includes/lostpw.php
+++ includes/lostpw.php
@@ -1,6 +1,16 @@
 <?php
 include "/hackathonfall14/includes/header.php";
 ?>
+<missionstatement>
+    <ms class="header">
+        Speak Easy
+    </ms>
+    <ms class="content">
+    <br>Making teamwork smarter and faster.
+    <br>Connect with classmates and peers and begin to work together!
+    <br>Solve the problems of today and reach the goals of tomorrow.
+    </ms>
+</missionstatement>
 <signup>
 <!--Basically, forums just need to use post and you have to let Jeff know the name of the inputs -->
 <!--This is a shitty mockup from Jeff, change it however you want -->
