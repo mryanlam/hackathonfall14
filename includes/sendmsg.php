c528b4b5a5a06a426ee9feb66127b2b64f373536
Jeff Goodhue <goodhue.jeff@gmail.com>
2014-11-02 06:43:07 -0800

    fix #4

--- includes/sendmsg.php
+++ includes/sendmsg.php
@@ -2,10 +2,10 @@
 if(isset($_POST['sendval'])) {
     $pquery = "INSERT INTO post (previousId, type, authId, crsId) VALUES (0, 1, ".$_SESSION['user'].", ".$_POST['sendval'].")";
     $pres = $dbc->query($pquery);
-    $gquery = "SELECT MAX(id) FROM post";
+    $gquery = "SELECT MAX(id) as max FROM post";
     $gres = $dbc->query($gquery);
     $grow = $gres->fetch_assoc();
-    $tquery = "INSERT INTO textpost (postId, message) VALUES (".$grow[0].", ".$_POST['msgtosend'].")";
+    $tquery = "INSERT INTO textpost (postId, message) VALUES (".$grow['max'].", ".$_POST['msgtosend'].")";
     $tres = $dbc->query($tquery);
 }
 ?>
