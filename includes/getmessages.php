f0fa5fb4d24313f2858beab06e10c3604f510f1e
Jeff Goodhue <goodhue.jeff@gmail.com>
2014-11-02 02:14:38 -0800

    Added ability to read messages... first attempt

---
+++ includes/getmessages.php
@@ -0,0 +1,29 @@
+<?php
+$query = "SELECT * FROM post WHERE previousId=0 AND (";
+$curclass = $classes2->fetch_assoc();
+$query = $query."crsId='".$curclass[crs1]."'";
+while($curclass = $classes2->fetch_assoc()) {
+    $query = $query." OR crsId='".$curclass[crs1]."'";
+}
+$query = $query.")";
+$result = $dbc->query($query);
+while($curmsg = $result->fetch_assoc()) {
+    $query1 = "SELECT courseName FROM course WHERE id=".$curmsg['crsId'];
+    $query2 = "SELECT first, last FROM account WHERE id=".$curmsg['authId'];
+    $result1 = $dbc->query($query1);
+    $result2 = $dbc->query($query2);
+    $crs = $result1->fetch_assoc();
+    $name = $result1->fetch_assoc();
+    print('<div class="topmsg">'.$name['first'].' '.$name['last'].' -> '.$crs['courseName'].'<br>');
+    if($curmsg['type'] == 1) {
+        $query1 = "SELECT message FROM textpost WHERE postId=".$curmsg['id'];
+        $result1 = $dbc->query($query1);
+        $msgtxt = $result1->fetch_assoc();
+        print($msgtxt['message'].'<br>');
+        //PRINT REPLIES AND ABILITY TO REPLY 
+    } else {
+        //Nothing as of yet... STAY TUNED!!
+    }
+    print('</div>');
+}
+?>
