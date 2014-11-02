29d4951527b1abe7afe9bbf3c3afa623222f64bc
Matthew Ryan Lam <lamm3@quest-OptiPlex-9010>
2014-11-02 00:40:11 -0700

    debugging password checking

--- includes/loginsc.php
+++ includes/loginsc.php
@@ -12,6 +12,8 @@ if(isset($_POST['submittedlog'])) {
     $result = $dbc->query($query);
     if($result->num_rows > 0) {
         $row = $result->fetch_assoc();
+        print($row['shaPassHash']);
+        print(hash("sha512", $_POST['userlog'].$_POST['passlog']));
         if(strcmp($row['shaPassHash'], hash("sha512", $_POST['userlog'].$_POST['passlog'])) == 0) {
             $_SESSION['loggedin'] = true;
             $_SESSION['first'] = $row['first'];
