a5cccb34d69c07bf49df1879265e8286d290c675
Matthew Ryan Lam <lamm3@quest-OptiPlex-9010>
2014-11-01 17:51:12 -0700

    added some elements that I forgot to use

--- htdocs/lostpwpage.php
+++ htdocs/lostpwpage.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-    include "/hackathonfall14/includes/landing.php";
+    header("Location: /hackathonfall14/htdocs/index.php");
 } else {
     include "/hackathonfall14/includes/lostpw.php";
 }
--- includes/lostpw.php
+++ includes/lostpw.php
@@ -26,10 +26,11 @@ hello
 <!--Basically, forums just need to use post and you have to let Jeff know the name of the inputs -->
 <!--This is a shitty mockup from Jeff, change it however you want -->
 <div>
-<form action="index.php" method="post">
+<form action="lostpwpage.php" method="post">
     <p>Lost Password!</p>
     <p><input type="text" name="email" placeholder="Email you signed up with"/></p>
     <p><input type="text" name="password" placeholder="New Password"/></p>
+    <p><input type="submit" name="submitlog" value="Login" /></p>
     <input type="hidden" name="submittedlog" value="TRUE" />
 </form>
 <?php
