d430b12d0d4ee7278b217035e8942a89122100c0
Jeff Goodhue <goodhue.jeff@gmail.com>
2014-11-01 21:49:15 -0700

    Forgot dollar

--- includes/getclasses.php
+++ includes/getclasses.php
@@ -1,7 +1,7 @@
 <?php
 foreach($classes as $curclass) {
     $query = "SELECT course.courseNumber, course.courseName, department.code FROM course INNER JOIN department ON course.deptId=department.id";
-    $result = dbc->query($query);
+    $result = $dbc->query($query);
     if($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         print('<div class="class" id='.$row['course.courseNumber'].'>');
