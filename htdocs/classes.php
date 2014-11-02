a96f68b5997ef5edc7c11464992bb270df0bc00e
Matthew Ryan Lam <lamm3@quest-OptiPlex-9010>
2014-11-01 19:30:18 -0700

    adding some basic functionality to adding classes

--- htdocs/classes.php
+++ htdocs/classes.php
@@ -3,6 +3,6 @@ session_start();
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     include "/hackathonfall14/includes/addclasses.php";
 } else {
-    header("Location: /hackathonfall14/htdocs/index.php");
+    header("Location: index.php");
 }
 ?>
