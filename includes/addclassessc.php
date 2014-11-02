2a81b39695e7f0834df6c5d750469c872e41949e
Matthew Ryan Lam <lamm3@quest-OptiPlex-9010>
2014-11-01 23:18:56 -0700

    new SQL query to insert into mycourses table

--- includes/addclassessc.php
+++ includes/addclassessc.php
@@ -1,22 +1,31 @@
 <?php
-    DEFINE('USER', 'root');
-    DEFINE('PASSWORD', '');
-    DEFINE('HOST', 'localhost');
-    DEFINE('DB', 'auth');
-    $dbc = new mysqli(HOST, USER, PASSWORD, DB);
-    if($dbc->connect_error) {
-        trigger_error("Could not connect! ".$dbc->connect_error, E_USER_ERROR);
-    }
+
+DEFINE('USER', 'root');
+DEFINE('PASSWORD', '');
+DEFINE('HOST', 'localhost');
+DEFINE('DB', 'auth');
+$dbc = new mysqli(HOST, USER, PASSWORD, DB);
+if($dbc->connect_error) {
+       trigger_error("Could not connect! ".$dbc->connect_error, E_USER_ERROR);
+}
+
+if (isset($_POST["choice"]))
+{
+    $query = 'INSERT INTO mycourses (id, crs1) VALUES ("'.$_SESSION["user"].'", "'.$_POST["choice"].'")';
+}
+else 
+{
     $query = "SELECT department.code, course.id, course.courseNumber, course.courseName FROM course INNER JOIN department ON course.deptid=department.id";
     //print($query);
     $result = $dbc->query($query);
     if($result) {
         while ($row = $result->fetch_assoc())
         {
-            printf("%s, %d, %s<br>", $row["code"], $row["courseNumber"], $row["courseName"]);
+            printf('<button onclick="chkBtn(%d)">%s, %d, %s</button><br>', $row["id"], $row["code"], $row["courseNumber"], $row["courseName"]);
         }
     } else {
         print("<p>No Classes Exist</p>");
     }
     $dbc->close();
+}
 ?>
