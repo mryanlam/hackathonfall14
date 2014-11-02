83a32127dd5caf1389c9bc38360ba89ca74e5a80
RON CHOI <ronald.firedragonx@gmail.com>
2014-11-01 23:10:30 -0700

    added JS to addclasses.php

--- includes/addclasses.php
+++ includes/addclasses.php
@@ -2,6 +2,21 @@
 include "/hackathonfall14/includes/header.php";
 ?>
 
+      <script language="javascript">         
+         function checkBtn(id) {
+               var form = document.createElement("form");
+               form.setAttribute("method", "post");
+               form.setAttribute("action", "classes.php");
+               var field = document.createElement("input");
+               field.setAttribute("type", "hidden");
+               field.setAttribute("name", "choice");
+               field.setAttribute("value", id);
+               form.appendChild(field);
+               document.body.appendChild(form);
+               form.submit();
+         }
+      </script>
+
 <classadd>
 <!--Basically, forums just need to use post and you have to let Jeff know the name of the inputs -->
 <!--This is a shitty mockup from Jeff, change it however you want -->
